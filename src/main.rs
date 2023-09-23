9976b8fa58fc914fdf11b33c3c5e5d7c796346e1
Tony Miller <mcfiredrill@gmail.com>
2023-09-23 14:22:33 -0700

    parse json spritesheet and animate it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::fs;
+use std::borrow::Borrow;
+
+use serde_json;
+use serde::{Deserialize, Serialize};
+
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
@@ -7,11 +13,49 @@ use sdl2::rect::{Point, Rect};
 use sdl2::image::{self, LoadTexture, InitFlag};
 use std::time::Duration;
 
+/* 
+ * shrimp model
+ *
+ * computer
+ *
+ * phone
+ */
+
 #[derive(Debug)]
 struct Shrimp {
     position: Point,
     sprite: Rect,
     current_frame: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Region {
+    x: i32,
+    y: i32,
+    w: i32,
+    h: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Margin {
+    x: i32,
+    y: i32,
+    w: i32,
+    h: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Sprite {
+    filename: String,
+    region: Region,
+    margin: Margin
+}
+
+fn parse_sprites_from_json(path: &str) -> Result<Vec<Sprite>, serde_json::Error> {
+    let data = fs::read_to_string(path).expect("Unable to read file");
+    let res: serde_json::Value = serde_json::from_str(&data).expect("Unable to parse");
+    let sprites: Result<Vec<Sprite>, serde_json::Error> = serde_json::from_value(res["textures"][0]["sprites"].borrow().clone());
+    return sprites;
 }
 
 fn render(
@@ -27,12 +71,13 @@ fn render(
 
     let (frame_width, frame_height) = shrimp.sprite.size();
     let current_frame = Rect::new(
-        shrimp.sprite.x() + frame_width as i32 * shrimp.current_frame,
+        shrimp.sprite.x() as i32,
         //shrimp.sprite.y() + frame_height as i32 * direction_spritesheet_row(shrimp.direction),
-        shrimp.sprite.y() + frame_height as i32,
+        shrimp.sprite.y() as i32,
         frame_width,
         frame_height,
         );
+    println!("current_frame rect: {:?}", current_frame);
 
 
     //canvas.copy(texture, None, None)?;
@@ -42,17 +87,26 @@ fn render(
     let screen_rect = Rect::from_center(screen_position, frame_width, frame_height);
     canvas.copy(texture, current_frame, screen_rect)?;
 
-
     canvas.present();
 
     Ok(())
 }
 
-fn update_shrimp(shrimp: &mut Shrimp) {
+fn update_shrimp(shrimp: &mut Shrimp, sprites: &Vec<Sprite>) {
     shrimp.current_frame = (shrimp.current_frame + 1) % 18;
+    println!("current_frame: {}", shrimp.current_frame);
+    let current_sprite = sprites.get(shrimp.current_frame as usize).expect("index out of bounds");
+    shrimp.sprite.x = current_sprite.region.x;
+    shrimp.sprite.y = current_sprite.region.y;
+    shrimp.sprite.w = current_sprite.region.w;
+    shrimp.sprite.h = current_sprite.region.h;
 }
 
 fn main() -> Result<(), String> {
+    let path = "assets/shrimpy.tpsheet";
+
+    let sprites: Vec<Sprite> = parse_sprites_from_json(&path).expect("Couldn't parse spritesheet");
+
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
     // Leading "_" tells Rust that this is an unused variable that we don't care about. It has to
@@ -94,7 +148,7 @@ fn main() -> Result<(), String> {
 
         // Update
         i = (i + 1) % 255;
-        update_shrimp(&mut shrimp);
+        update_shrimp(&mut shrimp, &sprites);
 
         // Render
         render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, &shrimp)?;
