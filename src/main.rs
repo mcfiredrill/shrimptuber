71b7aa114f5b0ccf00ddf6a0148a8e967b9523f6
Tony Miller <mcfiredrill@gmail.com>
2023-09-23 15:08:02 -0700

    bounce shrimp and add computer

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use sdl2::image::{self, LoadTexture, InitFlag};
 use std::time::Duration;
 
 /* 
- * shrimp model
+ * shrimp model r
  *
  * computer
  *
@@ -26,6 +26,7 @@ struct Shrimp {
     position: Point,
     sprite: Rect,
     current_frame: i32,
+    forward: bool,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -63,6 +64,7 @@ fn render(
     color: Color, 
     texture: &Texture,
     shrimp: &Shrimp,
+    computer_texture: &Texture,
 ) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
@@ -72,7 +74,6 @@ fn render(
     let (frame_width, frame_height) = shrimp.sprite.size();
     let current_frame = Rect::new(
         shrimp.sprite.x() as i32,
-        //shrimp.sprite.y() + frame_height as i32 * direction_spritesheet_row(shrimp.direction),
         shrimp.sprite.y() as i32,
         frame_width,
         frame_height,
@@ -86,6 +87,7 @@ fn render(
     let screen_position = shrimp.position + Point::new(width as i32 / 2, height as i32 / 2);
     let screen_rect = Rect::from_center(screen_position, frame_width, frame_height);
     canvas.copy(texture, current_frame, screen_rect)?;
+    canvas.copy(computer_texture, Rect::new(0, 0, 1920, 1080), Rect::new(-200, 200, 1920, 1080))?;
 
     canvas.present();
 
@@ -93,13 +95,29 @@ fn render(
 }
 
 fn update_shrimp(shrimp: &mut Shrimp, sprites: &Vec<Sprite>) {
-    shrimp.current_frame = (shrimp.current_frame + 1) % 18;
+    if shrimp.forward {
+        shrimp.current_frame = (shrimp.current_frame + 1) % 18;
+    } else {
+        shrimp.current_frame = (shrimp.current_frame - 1) % 18;
+    }
+    if shrimp.current_frame as usize == sprites.len() - 1 {
+        shrimp.forward = false;
+    } else if shrimp.current_frame == 0 {
+        shrimp.forward = true;
+    }
     println!("current_frame: {}", shrimp.current_frame);
     let current_sprite = sprites.get(shrimp.current_frame as usize).expect("index out of bounds");
+    println!("current_sprite: {:?}", current_sprite);
     shrimp.sprite.x = current_sprite.region.x;
     shrimp.sprite.y = current_sprite.region.y;
     shrimp.sprite.w = current_sprite.region.w;
     shrimp.sprite.h = current_sprite.region.h;
+    let y: f64 = 1.0;
+    if shrimp.forward {
+        shrimp.position.y = shrimp.position.y + (y.sin() * shrimp.current_frame as f64) as i32;
+    } else {
+        shrimp.position.y = shrimp.position.y - (y.sin() * shrimp.current_frame as f64) as i32;
+    }
 }
 
 fn main() -> Result<(), String> {
@@ -114,7 +132,7 @@ fn main() -> Result<(), String> {
     // temporary value and drop it right away!
     let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
 
-    let window = video_subsystem.window("game tutorial", 800, 600)
+    let window = video_subsystem.window("game tutorial", 1920, 1080)
         .position_centered()
         .build()
         .expect("could not initialize video subsystem");
@@ -124,12 +142,13 @@ fn main() -> Result<(), String> {
 
     let texture_creator = canvas.texture_creator();
     let texture = texture_creator.load_texture("assets/shrimpy-0.png")?;
-
+    let computer_texture = texture_creator.load_texture("assets/computer.png")?;
 
     let mut shrimp = Shrimp {
         position: Point::new(0, 0),
         sprite: Rect::new(0, 0, 286, 602),
         current_frame: 0,
+        forward: true,
     };
 
     let mut event_pump = sdl_context.event_pump()?;
@@ -151,7 +170,7 @@ fn main() -> Result<(), String> {
         update_shrimp(&mut shrimp, &sprites);
 
         // Render
-        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, &shrimp)?;
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, &shrimp, &computer_texture)?;
 
         // Time management!
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
